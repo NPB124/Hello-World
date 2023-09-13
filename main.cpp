770a5ea44a31bd97d5766a665906eaaa502f42ae
NPB124 <144799784+NPB124@users.noreply.github.com>
2023-09-13 11:16:28 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Nick Brett on 12/09/2023.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
